aab1b2219c2d5d370187a86d12103a13fc26d289
Mac Signer <macsigner@gmx.ch>
2022-05-19 18:22:41 +0200

    Add update source functionality

--- src/js/modules/compare.js
+++ src/js/modules/compare.js
@@ -3,6 +3,10 @@ class Compare {
         this.el = el;
         this.form = document.querySelector(el.dataset.compareFormSelector);
         this.form.addEventListener('submit', this._formSubmitDelegation.bind(this));
+        this.designSource = this.form.querySelector('#design-path');
+        this.mainSource = this.form.querySelector('#main-path');
+        this.image = this.el.querySelector('.compare__design img');
+        this.iframe = this.el.querySelector('.compare__main iframe');
 
         let toggleButton = this.form.querySelector('#toggle');
         toggleButton.addEventListener('click', () => {
@@ -12,6 +16,12 @@ class Compare {
         let diffCheckbox = this.form.querySelector('#diff');
         diffCheckbox.addEventListener("change", () => {
             this.el.classList.toggle("compare--diff");
+        });
+
+        let updateButton = this.form.querySelector('#update');
+        updateButton.addEventListener('click', () => {
+            this.image.src = this.designSource.value.trim() || this.image.src;
+            this.iframe.src = this.mainSource.value.trim() || this.iframe.src;
         });
     }
 

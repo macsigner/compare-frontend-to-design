b693718128a1ca830465efee963f7770057b2e09
Mac Signer <macsigner@gmx.ch>
2022-05-19 08:38:45 +0200

    Add toggle functionality

--- src/js/modules/compare.js
+++ src/js/modules/compare.js
@@ -2,9 +2,18 @@ class Compare {
     constructor(el) {
         this.el = el;
         this.form = document.querySelector(el.dataset.compareFormSelector);
+        this.form.addEventListener('submit', this._formSubmitDelegation.bind(this));
 
+        let toggleButton = this.form.querySelector('#toggle');
+        toggleButton.addEventListener('click', () => {
+            this.el.classList.toggle("compare--switch");
+        })
         console.log(this.el);
         console.log(this.form);
+    }
+
+    _formSubmitDelegation(e) {
+        e.preventDefault();
     }
 }
 
